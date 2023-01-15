30d18279d42bb4740c26b6c6a5be259d14397ca7
Ezgi Lena Sönmez <lena.sonmez@ug.bilkent.edu.tr>
2023-01-15 20:44:40 +0300

    coChangedFileFeatureCommit3

--- src/CoChangedFile1Ezgi.java
+++ src/CoChangedFile1Ezgi.java
@@ -5,7 +5,7 @@ public class CoChangedFile1Ezgi {
     public static void main(String[] args) {
 
         // Prints "Hello, World" in the terminal window.
-        System.out.println("CoChangedFile1Ezgi2");
+        System.out.println("CoChangedFile1Ezgi3");
     }
 
 }
