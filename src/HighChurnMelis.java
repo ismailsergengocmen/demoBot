cc0472108e8de830302173769a9763fa3847413a
Melo <63464796+melisatuun@users.noreply.github.com>
2023-01-15 20:36:00 +0300

    deneme 5

--- src/HighChurnMelis.java
+++ src/HighChurnMelis.java
@@ -8,5 +8,6 @@ public class HighChurnMelis {
         System.out.println("Hello, World");
         System.out.println("Hello, World");
         System.out.println("Hello, World");
+        System.out.println("Hello, World");
     }
 }
