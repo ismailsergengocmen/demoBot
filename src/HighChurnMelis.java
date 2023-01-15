37827195960825b5fae0b6607681d06b244c258b
Melo <63464796+melisatuun@users.noreply.github.com>
2023-01-15 20:33:29 +0300

    deneme 2

--- src/HighChurnMelis.java
+++ src/HighChurnMelis.java
@@ -3,9 +3,8 @@ package src;
 public class HighChurnMelis {
 
     public static void main(String[] args) {
-
         // Prints "Hello, World" in the terminal window.
         System.out.println("Hello, World");
+        System.out.println("Hello, World");
     }
-
 }
