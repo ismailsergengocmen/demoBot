4bda081420dc8eda0417c4c9485b6c8d98de0544
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2023-01-15 11:11:18 +0300

    coChangeSergen1

---
+++ src/CoChangedFile1Sergen.java
@@ -0,0 +1,7 @@
+package src;
+
+public class CoChangedFile1Sergen {
+  public void hi() {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/CoChangedFile2Sergen.java
@@ -0,0 +1,7 @@
+package src;
+
+public class CoChangedFile2Sergen {
+  public void hi() {
+    System.out.println("Hello World!");
+  }
+}
