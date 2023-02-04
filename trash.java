bdbaded7a8c463e11ccadaaf3ba5780e67933a32
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2023-02-05 00:05:04 +0300

    branch push test

--- trash.java
+++ trash.java
@@ -2,5 +2,6 @@ public class trash {
   public static void main(String[] args) {
     System.out.println("I am trash");
     System.out.println("I am trash");
+    System.out.println("I am trash");
   }
 }
