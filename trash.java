da5dbb6c4ee4c39f8c935e62c7ee08da22ea90cd
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2023-02-06 11:22:34 +0300

    push test

--- trash.java
+++ trash.java
@@ -7,5 +7,6 @@ public class trash {
     System.out.println("I am trash");
     System.out.println("I am trash");
     System.out.println("I am trash");
+    System.out.println("I am trash");
   }
 }
