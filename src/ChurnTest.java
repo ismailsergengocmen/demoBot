40f2de97846f3ec772f2012823a7b2f4c9a8fc00
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2023-02-19 16:40:34 +0300

    PR 2

--- src/ChurnTest.java
+++ src/ChurnTest.java
@@ -3,5 +3,9 @@ package src;
 public class ChurnTest {
   public static void main(String[] args) {
     System.out.println("PR 1");
+
+    System.out.println("PR 2");
+    System.out.println("PR 2");
+    System.out.println("PR 2");
   }
 }
